7dc07044099757a3b05e2f06220c1639eccf0314
Artur <arturrsilveira85@gmail.com>
2024-09-13 21:22:03 -0300

    Inicio da Página de Login

---
+++ src/app/(auth)/signin/page.tsx
@@ -0,0 +1,10 @@
+import { Logo } from "@/components/ui/logo";
+
+export default function Page() {
+  return (
+    <div className="max-w-lg mx-auto mt-12 px-6">
+      <Logo size={56} />
+      
+    </div>
+  )
+}
